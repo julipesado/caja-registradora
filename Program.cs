ffb5a425bd41c1af88e8b1700035282b07b99ec2
Julia Pesado <juliapesadoc@gmail.com>
2026-08-14 12:12:56 -0300

    etapa 1 y 2: bienvenida y carga de producto

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿const string NombreComercio = "KIOSCO JULI";
+Console.WriteLine($"=== {NombreComercio} ===");
+Console.Write("Nombre del cajero: ");
+string nombreCajero = Console.ReadLine();
+Console.WriteLine($"Bienvenida, {nombreCajero}. Caja abierta.");
+
+Console.Write("Nombre del producto: ");
+string nombreProducto = Console.ReadLine();
+Console.Write("Precio: ");
+decimal precioProducto = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine($"Producto cargado: {nombreProducto} - ${precioProducto}");
+
+
+Console.ReadLine();
