539d0ed2f4a85db8915e3b0ad95899d9b9fa2541
Julia Pesado <juliapesadoc@gmail.com>
2026-08-21 11:37:05 -0300

    etapa 3: carga multiple con menu

--- Program.cs
+++ Program.cs
@@ -4,11 +4,45 @@ Console.Write("Nombre del cajero: ");
 string nombreCajero = Console.ReadLine();
 Console.WriteLine($"Bienvenida, {nombreCajero}. Caja abierta.");
 
-Console.Write("Nombre del producto: ");
-string nombreProducto = Console.ReadLine();
-Console.Write("Precio: ");
-decimal precioProducto = Convert.ToDecimal(Console.ReadLine());
-Console.WriteLine($"Producto cargado: {nombreProducto} - ${precioProducto}");
+decimal total = 0;
+int cantidadProductos = 0;
+bool ventaAbierta = true;
 
+do
+{
+    Console.WriteLine();
+    Console.WriteLine("¿Qué desea hacer?");
+    Console.WriteLine("1 - Cargar un producto");
+    Console.WriteLine("2 - Cerrar la venta");
+    Console.Write("Opción: ");
+    string opcion = Console.ReadLine();
+
+
+    switch (opcion)
+    {
+        case "1":
+            Console.Write("Nombre del producto: ");
+            string nombreProducto = Console.ReadLine();
+            Console.Write("Precio: ");
+            decimal precioProducto = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Producto cargado: {nombreProducto} -- ${precioProducto}");
+            total += precioProducto;
+            cantidadProductos++;
+            break;
+
+        case "2":
+            ventaAbierta = false;
+            break;
+
+        default:
+            Console.WriteLine("Opción inválida.");
+            break;
+    }
+
+} while (ventaAbierta);
+
+Console.WriteLine();
+Console.WriteLine($"Productos cargados: {cantidadProductos}");
+Console.WriteLine($"Total: ${total}");
 
 Console.ReadLine();
